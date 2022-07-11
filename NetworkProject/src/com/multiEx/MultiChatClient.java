fd9dbc7cf8de964d8760d2010308c54b9325942e
Sanic00 <wodus48@gmail.com>
2022-07-11 10:03:17 +0900

    network

--- NetworkProject/src/com/multiEx/MultiChatClient.java
+++ NetworkProject/src/com/multiEx/MultiChatClient.java
@@ -68,6 +68,50 @@ public class MultiChatClient implements Runnable, ActionListener {
 		idInput = new JTextField(15);
 		loginButton = new JButton("로그인");
 		
+		//대화명 라벨 
+		label1 = new JLabel("대화명");
+		
+		//패널에 대한 레이아웃 설정
+		loginPanel.add(label1, BorderLayout.WEST);
+		loginPanel.add(idInput, BorderLayout.CENTER);
+		loginPanel.add(loginButton, BorderLayout.EAST);
+		
+		//로그아웃 패널 구성
+		logoutPanel = new JPanel();
+		
+		//로그아웃 레이아웃 설정
+		logoutPanel.setLayout(new BorderLayout());
+		
+		label2 = new JLabel();
+		logoutButton = new JButton("로그 아웃");
+		
+		logoutPanel.add(label2, BorderLayout.CENTER);
+		logoutPanel.add(logoutButton, BorderLayout.EAST);
+		
+		// 입력 패널 구성
+		msgPanel = new JPanel();
+		msgPanel.setLayout(new BorderLayout());
+		msgInput = new JTextField(30);
+				
+		exitButton = new JButton("종료");
+		
+		msgPanel.add(msgInput, BorderLayout.CENTER);
+		msgPanel.add(exitButton, BorderLayout.EAST);
+		
+		// 로그인 / 로그아웃 패널 선택을 위한 카드 레이아웃
+		tab = new JPanel();
+		clayout = new CardLayout();
+		tab.setLayout(clayout);
+		tab.add(loginPanel, "Login");
+		tab.add(logoutPanel, "Logout");
+	
+		// 메인 프레임 구성
+		jframe = new JFrame("멀티 채팅 프로그램"); //제목
+		msgOut = new JTextArea("", 10, 30); //"" = null로 잡은것
+		msgOut.setEditable(false); //사용자가 편집 수정 못하게 하는거
+		
+		// 수직 스크롤 바는 항상 나타태고, 수평 스크롤 바는 필요시에만 나타나기(내용이 많아지면 스크롤 바가 필요하다)
+		JScrollPane jsp = new JScrollPane();
 		
 		
 	}
