8fd74dbfc7006aa74e10e30dc7e859f2baf5d0f6
Sanic00 <105400221+Sanic00@users.noreply.github.com>
2022-07-12 10:44:30 +0900

    Add files via upload

--- EmpProject/src/com/empex/FindPane.java
+++ EmpProject/src/com/empex/FindPane.java
@@ -5,9 +5,9 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class AddPane extends JPanel implements ActionListener, ItemListener{
 
-	// 화면 관련 멤버 필드 정의 
+public class FindPane extends JPanel implements ActionListener {
+
 	private JPanel jp[] = new JPanel[6];
 	private JLabel jl[] = new JLabel[5];
 	private JTextField tf[] = new JTextField[5];
@@ -15,53 +15,45 @@ public class AddPane extends JPanel implements ActionListener, ItemListener{
 	private JButton okb; // ok버튼
 	private JButton rsb; //리셋버튼
 	
-	private int department = 10;
-	
+
 	// 제목 
-	String[] caption = {"이름:", "직책:","메일:","부서:"};
+	String[] caption = {"번호:","이름:", "직책:","메일:","부서:"};
 	
-	public AddPane() {
-		
-		setLayout(new GridLayout(6,1));
-		
+	
+	
+	public FindPane() {
+		//레이아웃 설정
+		setLayout(new GridLayout(7,1)); //caption이 한개 더 늘어나서 6 -> 7로 바꾼다
 		EtchedBorder eb = new EtchedBorder();
 		setBorder(eb);
 		
 		int size = caption.length;
 		//입력 받을 항목을 개수 
 		
-		
-		
-		// 텍스트 필드와 라벨을 패널에 묶어서 addPane에 추가
 		int i ;
-		for(i=0; i<size-1; i++){
+		for(i=0; i<size; i++){ //등록하는게 아니고 조회하는거라 size -1 을 하지않는다.
 			jp[i] = new JPanel();
 			jl[i] = new JLabel(caption[i]);
 			tf[i] = new JTextField(15);
+			
 			jp[i].add(jl[i]);
 			jp[i].add(tf[i]);
 			add(jp[i]);
 			
-		}
-		jp[i] = new JPanel();
-		jl[i] = new JLabel(caption[i]);
-		jp[i].add(jl[i]);
-		add(jp[i]);
-		
-		JComboBox combo = new JComboBox<>();
-		combo.addItem("부서번호 선택 하세요.");
-		for(int c = 1; c <=50; c++) {
-			combo.addItem(c*10); // 돌아갈때마다 10씩 
+			//텍스트 필드를 입력 불가능한 상태로 만듬
+			tf[i].setEditable(false); 
+			
+			//사원 번호 와 사원 이름은 검색해야 하므로 입력가능한 상태로 만든다.
+			if(i == 0 || i == 1)
+				tf[i].setEditable(true); 
+			
+			
 		}
 		
-		jp[i].add(combo);
-		
-		combo.addItemListener(this);
-		
-		
+
 		jp[size] = new JPanel();
 		
-		okb = new JButton("저장하기");
+		okb = new JButton("사원정보조회");
 		okb.addActionListener(this);
 		rsb = new JButton("다시쓰기");
 		rsb.addActionListener(this);
@@ -70,41 +62,21 @@ public class AddPane extends JPanel implements ActionListener, ItemListener{
 		
 		add(jp[size]);
 		
-	
-		
 	}
 	
 	
-	/*
-	 * itemStateChanged(ItemEvent e)
-	 * -comboBox의 내용을 선택할때 사용함
-	 * 
-	 */
-	
-	@Override
-	public void itemStateChanged(ItemEvent ie) {
-	if(ie.getStateChange() == ItemEvent.SELECTED)
-		department = Integer.parseInt(ie.getItem().toString()); //여기다 저장된다.
-		
-	}
-	
-	
-	
-	
-	/*
-	 *  actionPerformed(ActionEvent e)
-	 *  저장하기 버튼을 누르면 텍스트 필드의 내용을 데이터베이스에 저장함
-	 *  다시쓰기 버튼을 누르면 텍스트 필드를 초기화 함
-	 */
 	@Override
 	public void actionPerformed(ActionEvent ae) {
+		//이벤트가 발생한 버튼의 캡션 값을 얻어 옴
+		String ae_type = ae.getActionCommand(); 
 		
-		String ae_type = ae.getActionCommand(); //명령이 어떤건지
+		//검색한 사원정보를 저장한 VO객체 
 		EmployeeVO evo = null;
+		//DataBase 처리를 위한 DAO객체 
 		EmployeeDAO edvo = null;
 		
 		
-		if(ae_type.equals(okb.getText())) {
+		if(ae_type.equals(okb.getText())) {//조회버튼을 누를 경우
 			
 			try {
 				
@@ -123,26 +95,21 @@ public class AddPane extends JPanel implements ActionListener, ItemListener{
 				JOptionPane.showMessageDialog(this, tf[0].getText() + "님이 성공적으로 추가했습니다.");
 				
 			
-		}else if(ae_type.equals(rsb.getText())) {
+		}else if(ae_type.equals(rsb.getText())) { // 다시 쓰기 버튼을 누를경우
 			int size = caption.length;
 			for(int i =0; i < size-1; i++) {
 				tf[i].setText("");
 			}
 		}
-			
 		
 		
+		
+		
+		
+		
+		
+		
+
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
